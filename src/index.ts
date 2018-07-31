95dbf35895b378f56c410184925bc29480d61f41
ryota suzuki <ryota@fringe81.com>
2018-07-31 11:44:31 +0900

    fp-tsが使える場を整える

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,24 @@
-console.log("HW");
+import * as fp from "fp-ts";
+
+function toNumber(value: any): fp.option.Option<number> {
+  const number = Number(value);
+  if (!isNaN(number)) {
+    return fp.option.some(number);
+  } else {
+    return fp.option.none;
+  }
+}
+
+const strOpt = fp.option.fromNullable(10000);
+const numberValueOpt = strOpt.chain(toNumber);
+
+const number = numberValueOpt.getOrElse(0);
+
+console.log(number);
 
 function hoge(arg: number) {
   console.log(arg);
 }
 
-hoge("#");
+
+

1c1cf91bb0532096c1642ccc005a514e90579aa1
ryota suzuki <ryota@fringe81.com>
2018-08-02 20:11:56 +0900

    WIP: チョロ実装

--- queryGen.js
+++ queryGen.js
@@ -1,14 +1,48 @@
-var chokidar = require('chokidar')
-var watcher = chokidar.watch('./queries', {	//watch対象ディレクトリorファイル
+const spawn = require('child_process').spawnSync;
+const fs = require('fs');
+const chokidar = require('chokidar');
+const watcher = chokidar.watch('./queries', {	//watch対象ディレクトリorファイル
 	ignored: /\.ts|__generated__/,	//無視する対象
 	persistent:true	//監視を継続するかどうか
-	})
- 
- 
-watcher.on('ready', () => { console.log("監視開始"); })
-	.on('add', (path) => { console.log("追加ファイル-> " + path); })
-	.on('addDir', (path) => { console.log("追加ディレクトリ-> " + path); })
-	.on('unlink', (path) => { console.log("削除されました-> " + path);　/* TODO: __generated__以下を全部消す。 ./node_modules/apollo/bin/run codegen:generate --target=typescript --schema=schema.json --queries="queries/*.graphql"を叩く */  })
-  .on('unlinkDir', (path) => { console.log("削除されました-> " + path);/* TODO: __generated__以下を全部消す。 ./node_modules/apollo/bin/run codegen:generate --target=typescript --schema=schema.json --queries="queries/*.graphql"を叩く */ })
-	.on('change', (path) => { console.log("修正されました-> " + path); /* TODO: ./node_modules/apollo/bin/run codegen:generate --target=typescript --schema=schema.json --queries="queries/*.graphql"を叩く */ })
-	.on('error', (error) => { console.log("エラーです-> " + error); })
+	});
+
+function generateTsDef() {
+	spawn('./node_modules/apollo/bin/run', 
+		['codegen:generate', '--target=typescript', '--schema=schema.json', '--queries="queries/*.graphql"'],
+		{stdio: 'inherit'}
+	)
+}
+generateTsDef();
+function clearGenerateDir() {
+	if (!fs.existsSync('queries/__generated__')) return -1;
+	console.log('------- start clear directory __gererated__ -------');
+	const allFileNames = fs.readdirSync('queries/__generated__');
+	allFileNames.forEach(fileName => {
+		fs.unlinkSync(`queries/__generated__/${fileName}`);
+	});
+	fs.rmdirSync('queries/__generated__');
+	console.log('------- finish clear directory __gererated__ -------');
+}
+
+// watcher.on('ready', () => { console.log("監視開始"); })
+// 	.on('add', (path) => { 
+// 		console.log("追加ファイル-> " + path); 
+// 	})
+// 	.on('addDir', (path) => {
+// 		 console.log("追加ディレクトリ-> " + path); 
+// 		})
+// 	.on('unlink', (path) => { 
+// 		console.log("削除されました-> " + path);
+// 		clearGenerateDir();
+// 		generateTsDef();
+// 	})
+//   .on('unlinkDir', (path) => {
+// 		 console.log("削除されました-> " + path);
+// 		 clearGenerateDir();
+// 		 generateTsDef();
+// 	})
+// 	.on('change', (path) => {
+// 		 console.log("修正されました-> " + path); 
+// 		 generateTsDef();
+// 	})
+// 	.on('error', (error) => { console.log("エラーです-> " + error); });
